3e4daf563ba5a769f1103b412744485cca258f98
ArtemElectr <71963675+ArtemElectr@users.noreply.github.com>
2024-06-15 15:56:51 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,23 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+
+# 1 вариант
+while my_list[i] >= 0 and i < len(my_list):
+    if my_list[i] == 0:
+        i += 1
+        continue
+    print(my_list[i])
+    i += 1
+
+# 2 вариант
+print(' ')
+i = 0
+while i < len(my_list):
+    if my_list[i] < 0:
+        break
+    elif my_list[i] > 0:
+        print(my_list[i])
+        i += 1
+    else:
+        i += 1
+        continue
