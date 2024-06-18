fe773d59f7b9de1b8f0f330ed97ba36415b54182
ArtemElectr <71963675+ArtemElectr@users.noreply.github.com>
2024-06-18 17:23:26 +0300

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -1,6 +1,8 @@
 
 
 def search_pair_of_numbers(input_value):
+    if input_value < 3 or input_value > 20:
+        return 0
     list_summ = []
     for i in range(1, input_value):
         for j in range(1, input_value):
@@ -17,4 +19,9 @@ def search_pair_of_numbers(input_value):
 while True:
     value_ = int(input("Введите любое число от 3 до 20: "))
     result = search_pair_of_numbers(value_)
+
+    if result == 0:
+        print(f"Число {value_} не входит в диапазон от 3 до 20")
+        continue
+        
     print(*result)
