14a3966471ab12bb4468de5dfb406e48f4ac2bc1
ArtemElectr <71963675+ArtemElectr@users.noreply.github.com>
2024-07-06 20:10:28 +0300

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,31 @@
+class House:
+    def __init__(self, name, number_of_floor):
+        self.name = name
+        self.number_of_floor = number_of_floor
+
+    def go_to(self, new_floor):
+        new_floor = int(new_floor)
+
+        if self.number_of_floor >= new_floor >= 1:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print('Такого этажа не существует')
+
+    def __len__(self):
+        return self.number_of_floor
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floor}"
+
+
+h1 = House("ЖК Эльбрус", 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
