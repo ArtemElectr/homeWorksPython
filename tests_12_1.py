aa0ab178ab6155a321a02f8d661bc8d985bbebfa
ArtemElectr <71963675+ArtemElectr@users.noreply.github.com>
2024-09-13 10:22:49 +0300

    Update tests_12_1.py

--- tests_12_1.py
+++ tests_12_1.py
@@ -8,14 +8,14 @@ class RunnerTest(tc):
         for i in range(10):
             new_runner.walk()
 
-        tc.assertEqual(self, new_runner.distance, 50)
+        self.assertEqual(new_runner.distance, 50)
 
     def test_run(self):
         new_runner = runner.Runner('Kate')
         for i in range(10):
             new_runner.run()
 
-        tc.assertEqual(self, new_runner.distance, 100)
+        self.assertEqual(new_runner.distance, 100)
 
     def test_challenge(self):
         runner1, runner2 = runner.Runner('Ann'), runner.Runner('Alex')
@@ -23,7 +23,7 @@ class RunnerTest(tc):
             runner1.run()
             runner2.walk()
 
-        tc.assertNotEqual(self, runner1.distance, runner2.distance)
+        self.assertNotEqual(runner1.distance, runner2.distance)
 
 
 if __name__ == '__main__':
